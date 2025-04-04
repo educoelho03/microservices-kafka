d150baaa5479ad0c1741a9b11b6920a74d7c5dd3
educoelho03 <eduardopaivacoelho@outlook.com>
2025-04-04 14:23:10 -0300

    configuring microservices producers

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/core/utils/JsonUtil.java
@@ -0,0 +1,32 @@
+package br.com.microservices.orchestrated.orchestratorservice.core.utils;
+
+import br.com.microservices.orchestrated.orchestratorservice.core.dto.Event;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class JsonUtil {
+
+    private final ObjectMapper objectMapper;
+
+    public JsonUtil(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public String toJson(Object obj) {
+        try {
+            return objectMapper.writeValueAsString(obj);
+        } catch (Exception e){
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception ex){
+            return null;
+        }
+    }
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/dto/History.java
@@ -0,0 +1,18 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class History {
+
+    private String source;
+    private String status;
+    private String message;
+    private LocalDateTime createdAt;
+}
